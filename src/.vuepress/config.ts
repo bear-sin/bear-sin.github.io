1ffc36830f72e852429d93872c300a8e31588621
bear-sin <2282443447@qq.com>
2023-08-06 15:09:17 +0800

    第一次上传

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,15 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "Sin的博客",
+  description: "Sin 的博客演示",
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,50 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  "/demo/",
+  {
+    text: "日常",
+    icon: "pen-to-square",
+    prefix: "/posts/",
+    children: [
+      {
+        text: "With_Deer",
+        icon: "pen-to-square",
+        prefix: "With_Deer/",
+        children: [
+          { text: "出去玩", icon: "pen-to-square", link: "1" }
+
+        ],
+      },
+      {
+        text: "香蕉",
+        icon: "pen-to-square",
+        prefix: "banana/",
+        children: [
+          {
+            text: "香蕉 1",
+            icon: "pen-to-square",
+            link: "1",
+          },
+          {
+            text: "香蕉 2",
+            icon: "pen-to-square",
+            link: "2",
+          },
+          "3",
+          "4",
+        ],
+      },
+      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+      "tomato",
+      "strawberry",
+    ],
+  },
+  {
+    text: "V2 文档",
+    icon: "book",
+    link: "https://theme-hope.vuejs.press/zh/",
+  },
+]);
