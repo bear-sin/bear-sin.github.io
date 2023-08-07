ce9e53e959455ba77193b22fac12cc00a33ee9da
bear-sin <2282443447@qq.com>
2023-08-07 10:30:37 +0800

    'deploy'

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -12,32 +12,9 @@ export default navbar([
         text: "With_Deer",
         icon: "pen-to-square",
         prefix: "With_Deer/",
-        config:  {
-              // 这会加密整个 guide 目录，并且两个密码都是可用的
-              "/guide/": ["1234", "5678"],
-            },
         children: [
-          { text: "出去玩", icon: "pen-to-square", link: "1" }
+          { text: "OUTING", icon: "pen-to-square", link: "1" }
 
-        ],
-      },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
         ],
       },
       { text: "樱桃", icon: "pen-to-square", link: "cherry" },
