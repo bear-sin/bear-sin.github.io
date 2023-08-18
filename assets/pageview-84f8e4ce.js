9358fd7cf431807f25cd5e670af0627db8e2306c
bear-sin <115936233+bear-sin@users.noreply.github.com>
2023-08-18 08:55:45 +0000

    Deploying to gh-pages from @ bear-sin/bear-sin.github.io@b0a669f2b9f96ccd4c41cc5e5214f5301fe01137 🚀

--- assets/pageview-84f8e4ce.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./app-2bc4bd20.js";const a=()=>e({serverURL:"https://waline-comment.vuejs.press"});export{a as updatePageview};
---
+++ assets/pageview-f9b178d7.js
@@ -0,0 +1,1 @@
+import{P as e}from"./app-b2ccdbac.js";const a=()=>e({serverURL:"https://waline-comment.vuejs.press"});export{a as updatePageview};
